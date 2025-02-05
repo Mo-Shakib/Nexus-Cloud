dfe293a5221bbdcea12b2de10653c886b5e9d797
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2025-02-05 17:42:35 +0600

    gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: typeof process !== 'undefined' && process.env.NODE_ENV === 'production' ? '/Nexus-Cloud' : '',
+  assetPrefix: typeof process !== 'undefined' && process.env.NODE_ENV === 'production' ? '/Nexus-Cloud/' : '',
 };
 
 export default nextConfig;
